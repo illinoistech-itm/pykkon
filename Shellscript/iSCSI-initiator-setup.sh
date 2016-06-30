421f25d342b595693f331433140c1053c3eacc77
lucasdeassis <lucasassis413@gmail.com>
2016-06-30 13:12:56 -0500

    update initiator script and Vagrantfiles to include setup script download.

--- Shellscript/iSCSI-initiator-setup.sh
+++ Shellscript/iSCSI-initiator-setup.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 # iSCSI Initiator setup
 
-umount /media/william/97a89aa9-7ae1-4641-87dd-593ea1fc525b
 echo Insert target IP address:
 read ip
 sudo iscsiadm -m node --logout
 sudo iscsiadm -m discovery -t st -p $ip
 sudo iscsiadm -m node -T "iqn.2016-06.local.iit:storage.sys0" -p "$ip:3260" --login
-sudo mount -t ext3 /dev/sdb
+
+if [ -e /dev/sdb ]; then
+	sudo mount /dev/sdb /mnt
+elif [ -e /dev/sdc ]; then
+	sudo mount /dev/sdc /mnt
+elif [ -e /dev/sdd ]; then
+	sudo mount /dev/sdd /mnt
+fi
