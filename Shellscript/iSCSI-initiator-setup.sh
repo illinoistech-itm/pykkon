7e93aaa081c90bd4b5bf82b99f406fd4c7b3995a
Matheus Pavan <Matheus Pavan>
2016-07-05 15:58:41 -0400

    Update iSCSI-initiator-setup.sh

--- Shellscript/iSCSI-initiator-setup.sh
+++ Shellscript/iSCSI-initiator-setup.sh
@@ -7,4 +7,4 @@ sudo iscsiadm -m node --logout
 sudo iscsiadm -m discovery -t st -p $ip
 sudo iscsiadm -m node -T "iqn.2016-06.local.iit:storage.sys0" -p "$ip:3260" --login
 
-sudo mount /dev/sdc /mnt || sudo mount /dev/sdd /mnt
+sudo mount /dev/sdc /mnt || sudo mount /dev/sdc /mnt || sudo mount /dev/sdd /mnt || sudo mount /dev/sdd /mnt
