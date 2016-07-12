dd13d1426375965ab53b710fbfc063b0f2784848
lucasdeassis <lucasassis413@gmail.com>
2016-07-12 13:06:01 -0500

    documentation on initiator and target scripts.

--- Shellscript/iSCSI-initiator-setup.sh
+++ Shellscript/iSCSI-initiator-setup.sh
@@ -7,4 +7,5 @@ sudo iscsiadm -m node --logout
 sudo iscsiadm -m discovery -t st -p $ip
 sudo iscsiadm -m node -T "iqn.2016-06.local.iit:storage.sys0" -p "$ip:3260" --login
 
+# as this script containts mounting commands, do not run on the script mount point folder (/mnt)
 sudo mount /dev/sdc /mnt || sudo mount /dev/sdc /mnt || sudo mount /dev/sdd /mnt || sudo mount /dev/sdd /mnt
--- Shellscript/iSCSI-target-setup.sh
+++ Shellscript/iSCSI-target-setup.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 # iSCSI Target machine Script to be run after modifying the file in the LUN 0.
 
-# First create the Disk Storage:
-
+# First create the file system:
 sudo /sbin/mkfs -t ext4 /dev/sda1p3 2M
 
 # Mount the disk storage (umount if it is already mounted) and create test file:
 sudo rm /data/*.txt
+
+#umount before trying mount
 sudo umount /dev/sda1p3
+# as this script containts mounting commands, do not run on the script mount point folder (/data)
 sudo mount /dev/sda1p3 /data/
 sudo echo "Test ISCSI File" > /data/test.txt
 
