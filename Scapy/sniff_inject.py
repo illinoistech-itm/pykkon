93d27ecbd702cec07f81b54c47768e44e863b842
lucasdeassis <lucasassis413@gmail.com>
2016-07-20 18:15:24 -0500

    reduce # of packets sniffed.

--- Scapy/sniff_inject.py
+++ Scapy/sniff_inject.py
@@ -4,11 +4,10 @@ import os
 running = True
 while(running):
 	# -c 650 are the packets we want to sniff. 650 goes until test file
-	packet_read = os.popen('hexinject -s -i eth0 -c 650 -f tcp').read()
+	packet_read = os.popen('hexinject -s -i eth0 -c 550 -f tcp').read()
 	print packet_read + " *********************** "
 	packet_list = packet_read.splitlines()
 	for packet in packet_list:
-		
 		# below we look for the 'test iscsi file' in hex
 		if ('54 65 73 74 20 49 53 43 53 49 20 46 69 6C 65' in packet):
 			print "iscsi content file identified!"
