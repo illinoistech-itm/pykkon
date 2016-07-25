083a83c1a24ed467dfc47ae6b97ecc93e6c64fca
System Administrator <root@dhcp60.ssv2.iit.edu>
2016-07-25 00:13:47 -0500

    increase mtu and wait time before mount on setup.

--- Shellscript/iSCSI-initiator-setup.sh
+++ Shellscript/iSCSI-initiator-setup.sh
@@ -7,5 +7,7 @@ sudo iscsiadm -m node --logout
 sudo iscsiadm -m discovery -t st -p $ip
 sudo iscsiadm -m node -T "iqn.2016-06.local.iit:storage.sys0" -p "$ip:3260" --login
 
+# wait time for disk creation
+sleep 1
 # as this script containts mounting commands, do not run on the script mount point folder (/mnt)
 sudo mount /dev/sdc /mnt || sudo mount /dev/sdc /mnt || sudo mount /dev/sdd /mnt || sudo mount /dev/sdd /mnt
